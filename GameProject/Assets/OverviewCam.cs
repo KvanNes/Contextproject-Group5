0a05c09cb1c367b1b043a82ece31d6460fe72a6d
Kevin van Nes <kevinvnes@gmail.com>
2014-05-20 12:27:44 +0200

    First working version of game with minimap. Collaborator(s): Kevin

---
+++ GameProject/Assets/OverviewCam.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OverviewCam : MonoBehaviour {
+
+	public Transform target;
+	public const float damping = 6.0f;
+	public bool smooth = true;
+
+	void LateUpdate () {
+		if(target) {
+			if(smooth) {
+				Quaternion rotation = Quaternion.LookRotation(target.position - transform.position);
+				transform.rotation = Quaternion.Slerp(transform.rotation, rotation, damping * Time.deltaTime);
+			} else {
+				transform.LookAt(target);
+			}
+			Vector3 vec = new Vector3(target.position.x, target.position.y, target.position.z);
+			transform.position = vec;
+		}
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		if(rigidbody)
+			rigidbody.freezeRotation = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
