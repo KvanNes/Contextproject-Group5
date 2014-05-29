390ed15b2834bb8d98f5b6fe7fc87470c55dff46
melmaouchi <m.elmaouchi@student.tudelft.nl>
2014-05-29 14:32:58 +0200

    Add the Mock-folder

---
+++ GameProject/Assets/Architecture/Mock/INetwork.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface INetwork
+{
+    object Instantiate(UnityEngine.Object prefab, Vector3 location, Quaternion rotation, int group);
+    NetworkConnectionError InitializeServer(int maxConnection, int portnumber, bool NATPunchthrough);
+    void Disconnect();
+}
---
+++ GameProject/Assets/Architecture/Mock/NetworkWrapper.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System;
+
+public class NetworkWrapper : INetwork
+{
+
+    public string IPAdress;
+    public bool connected;
+
+    public object Instantiate(UnityEngine.Object prefab, Vector3 location, Quaternion rotation, int group)
+    {
+        return Network.Instantiate(prefab, location, rotation, group);
+    }
+
+    public NetworkConnectionError InitializeServer(int maxConnection, int portnumber, bool NATPunchthrough)
+    {
+        return Network.InitializeServer(maxConnection, portnumber, NATPunchthrough);
+    }
+
+    public void Disconnect()
+    {
+        Network.Disconnect();
+    }
+}
