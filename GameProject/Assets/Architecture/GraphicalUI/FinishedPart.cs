b833b49e68843caab43e95d926e9d5701ee49ad2
Ramin Erfani <R.SafarpourErfani@student.tudelft.nl>
2014-06-16 10:59:02 +0200

    Fixed label positions and sizes

--- GameProject/Assets/Architecture/GraphicalUI/FinishedPart.cs
+++ GameProject/Assets/Architecture/GraphicalUI/FinishedPart.cs
@@ -10,12 +10,12 @@ namespace GraphicalUI {
 			if (MainScript.SelfCar != null) {
 				if (MainScript.SelfCar.CarObject.state == Behaviours.AutoBehaviour.FinishedState.won) {
 					GUI.Label (
-						new Rect (Screen.width - 50, 0, 50, 30),
+						new Rect ((Screen.width / 2) - 100, (Screen.width / 2) - 50, 200, 100),
 						new GUIContent ("YOU WON")
 					);
 				} else if (MainScript.SelfCar.CarObject.state == Behaviours.AutoBehaviour.FinishedState.lost) {
 					GUI.Label (
-						new Rect (Screen.width - 50, 0, 50, 30),
+						new Rect ((Screen.width / 2) - 100, (Screen.width / 2) - 50, 200, 100),
 						new GUIContent ("YOU LOST")
 					);
 				}
