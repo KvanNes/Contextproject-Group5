24091a03eb87737710e2ac21742d2a1a97723325
Coorlob <luijendijk12@hotmail.com>
2014-06-17 12:14:31 +0200

    startscreen added

--- GameProject/Assets/Architecture/GraphicalUI/MainPart.cs
+++ GameProject/Assets/Architecture/GraphicalUI/MainPart.cs
@@ -9,30 +9,38 @@ using Utilities;
 
 namespace GraphicalUI {
     public class MainPart : GraphicalUIPart {
-        
-        private static int buttonX = 10;
-        private static int buttonY = 75;
-        private static int buttonW = 150;
-        private static int buttonH = 50;
+		
+		private static int buttonX = Screen.width / 2;// + Screen.width / 10 * 4 ;
+        private static int buttonY = Screen.height / 2 ;
+        private static int buttonW = Screen.width / 8; //150;
+		private static int buttonH = Screen.width / 10; //50;
+		
         
         private void CreateServerButton() {
-            if (GUI.Button(new Rect(Screen.width / 2 - 75, Screen.height / 2 - 75, 150, 150), "Start server!")) {
-                MainScript.SelfType = MainScript.PlayerType.Server;
-                MainScript.Server.StartServer();
-                NetworkController.connected = true;
-            }
+			//if (!ServerAvailable()) {
+				GUI.color = Color.yellow;
+				GUI.backgroundColor = Color.green;
+				if (GUI.Button (new Rect (Screen.width, Screen.height / 2 - Screen.height /5, Screen.width / 10, Screen.width / 10), "Start server!")) {
+								MainScript.SelfType = MainScript.PlayerType.Server;
+								MainScript.Server.StartServer ();
+								NetworkController.connected = true;
+						}
+				//}
         }
         
         private void CreateClientButton(Type type, HostData hostData, int carNumber, int x, int y) {
-            if (GUI.Button(new Rect(x, y, buttonW, buttonH), type.Name + "; Car " + carNumber)) {
+			string teamColorText = carNumber == 0 ? "Team Red\n" : "Team Blue\n";
+			GUI.color = Color.yellow;
+			GUI.backgroundColor = Color.green;
+            if (GUI.Button(new Rect(x, y, buttonW, buttonH), teamColorText + type.Name)) {
                 MainScript.SelfType = MainScript.PlayerType.Client;
                 MainScript.SelfCar = new Car(carNumber);
                 
                 // Gebaseerd op: http://stackoverflow.com/a/755
                 MainScript.SelfPlayer = new Player(MainScript.SelfCar, (IPlayerRole) Activator.CreateInstance(type));
                 MainScript.SelfPlayer.Role.Initialize();                
-                
-                MainScript.Client.ChooseJobWhenConnected(type.Name, carNumber);
+				GameObject.FindGameObjectWithTag("BackgroundCamera").camera.enabled = false;
+				MainScript.Client.ChooseJobWhenConnected(type.Name, carNumber);
                 Network.Connect(hostData);
                 NetworkController.connected = true;
             }
@@ -40,12 +48,12 @@ namespace GraphicalUI {
         
         private void CreateClientButtons(int startX, int startY, HostData hostData) {
             for(int j = 0; j < GameData.CARS_AMOUNT; j++) {
-                int y = startY + j * buttonH;
+				int y = startY + j * buttonH;
                 IEnumerable<Type> roleTypes = Utils.TypesImplementingInterface(typeof(IPlayerRole));
                 int roleCounter = 0;
                 foreach(Type role in roleTypes) {
-                    CreateClientButton(role, hostData, j, startX + roleCounter, y);
-                    roleCounter += buttonW;
+					CreateClientButton(role, hostData, j, startX + roleCounter, y);
+					roleCounter += buttonW;
                 }
             }
         }
@@ -66,7 +74,7 @@ namespace GraphicalUI {
             }
             
             // Gebaseerd op: http://answers.unity3d.com/questions/296204/gui-font-size.html
-            GUI.skin.label.fontSize = GUI.skin.button.fontSize = 20;
+            //GUI.skin.label.fontSize = GUI.skin.button.fontSize = 20;
             
             if (!Network.isServer) {
                 CreateServerButton();
@@ -78,7 +86,7 @@ namespace GraphicalUI {
             if (ServerAvailable() && !Network.isServer && !Network.isClient) {
                 for (int i = 0; i < NetworkController.hostData.Length; i++) {
                     CreateClientButtons(buttonX, buttonY_, NetworkController.hostData[i]);
-                    buttonY_ += buttonH * GameData.CARS_AMOUNT + 30;
+                    buttonY_ += buttonH * GameData.CARS_AMOUNT;
                 }
             }
         }
